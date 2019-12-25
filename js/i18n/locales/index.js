41e5dc64a5180d8a0721a48da0465f1ac487f0f1
xiangxia <xia.xiang@mindertech.co.uk>
2019-12-25 16:00:06 +0800

    1

--- js/i18n/locales/index.js
+++ js/i18n/locales/index.js
@@ -1,10 +1,11 @@
 import I18n from 'react-native-i18n';
+import DeviceInfo from 'react-native-device-info';
 import en from './en';
 import zh from './zh';
 
 I18n.fallbacks = true;
 
-I18n.defaultLocale = 'en';
+I18n.defaultLocale = DeviceInfo.getDeviceLocale();
 
 I18n.translations = {
   en,
