4354b13111120d6085d01eda5a2f6f96436c64f0
xiangxia <xia.xiang@mindertech.co.uk>
2019-12-25 15:56:00 +0800

    add react-native-i18n

---
+++ js/i18n/locales/index.js
@@ -0,0 +1,14 @@
+import I18n from 'react-native-i18n';
+import en from './en';
+import zh from './zh';
+
+I18n.fallbacks = true;
+
+I18n.defaultLocale = 'en';
+
+I18n.translations = {
+  en,
+  zh,
+};
+
+export default I18n;
--- js/test/test1.js
+++ js/test/test1.js
@@ -1,20 +1,59 @@
 import React from 'react';
 import {createAppContainer} from 'react-navigation';
 import {createStackNavigator} from 'react-navigation-stack';
-import {Text, View} from 'react-native';
-import {Component} from 'react-native';
+import {Component, View, StyleSheet, Text} from 'react-native';
+import {Container, Header, Content, Button} from 'native-base';
+import I18n from '../i18n/locales/index';
 
-const HomeScreen = () => (
-  <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
-    <Text>Home Screen</Text>
-  </View>
-);
+class HomeScreen extends React.Component {
+  constructor(props) {
+    super(props);
 
-const DetailScreen = () => (
-  <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
-    <Text>Detail Screen</Text>
-  </View>
-);
+    this.state = {
+      localeLanguage: null,
+    };
+  }
+  changeLanguage = () => {
+    I18n.locale = 'en';
+    this.setState({
+      localeLanguage: I18n.locale,
+    });
+  };
+  render() {
+    return (
+      // eslint-disable-next-line react-native/no-inline-styles
+      <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
+        <Button
+          title="Go To Details"
+          onPress={() => this.props.navigation.navigate('Detail')}>
+          <Text>Go To Details</Text>
+        </Button>
+        <Button
+          style={styles.button}
+          title="Change Language"
+          onPress={this.changeLanguage}>
+          <Text>Change Language</Text>
+        </Button>
+        <Text style={styles.container}>Home Screen1</Text>
+        <Text style={styles.container}>Home Screen2</Text>
+        <Text style={styles.container}>{I18n.t('greeting')}</Text>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: 'green',
+    marginTop: 20,
+    color: 'white',
+  },
+  button: {
+    marginTop: 20,
+  },
+});
+
+const DetailScreen = () => <View />;
 
 const routeConfigs = {
   Home: {
